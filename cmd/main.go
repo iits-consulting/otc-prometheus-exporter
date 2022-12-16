49a9cb55c29513a641e70325ee90b711baf4cc02
Erjon Dibrani <erjon@cloudastro.de>
2022-12-16 17:59:04 +0100

    Loading otc-auth-config

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/iits-consulting/otc-prometheus-exporter/internal"
 )
 
 func main() {
-	fmt.Println("GO RUNS")
-	internal.Loader()
+	const path = "~/.otc-auth-config"
+	config, err := internal.LoadConfigFromFile(path)
+	if err != nil {
+		panic(err)
+	}
+	projectToken, err := internal.GetScopedToken(*config, "eu-de_iits-infra")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(projectToken)
 }
--- internal/otc-config-loader.go
+++ internal/otc-config-loader.go
@@ -1,7 +1,84 @@
 package internal
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/user"
+	"path/filepath"
+	"strings"
+)
 
-func Loader() {
-	fmt.Println("GO RUNS INSIDE OTC_CONFIG_LOADER!!")
+type Config struct {
+	Clouds []Cloud
+}
+
+type Domain struct {
+	Name string `json:"name"`
+	Id   string `json:"id"`
+}
+
+type Token struct {
+	Secret    string `json:"secret"`
+	IssuedAt  string `json:"issued_at"`
+	ExpiresAt string `json:"expires_at"`
+}
+
+type Project struct {
+	Name        string `json:"name"`
+	Id          string `json:"id"`
+	ScopedToken Token
+}
+
+type Cluster struct {
+	Name string `json:"name"`
+	Id   string `json:"id"`
+}
+
+type Cloud struct {
+	Domain        Domain
+	UnscopedToken Token
+	Projects      []Project
+	Clusters      []Cluster
+}
+
+func expandUserHome(path string) string {
+
+	usr, _ := user.Current()
+	dir := usr.HomeDir
+
+	if strings.HasPrefix(path, "~/") {
+		path = filepath.Join(dir, path[2:])
+	}
+	return path
+}
+
+func LoadConfigFromFile(path string) (*Config, error) {
+	data, err := os.ReadFile(expandUserHome(path))
+	if err != nil {
+		return nil, err
+	}
+
+	var config Config
+
+	err = json.Unmarshal(data, &config)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func GetScopedToken(c Config, projectname string) (*Token, error) {
+	for _, cloud := range c.Clouds {
+		for _, project := range cloud.Projects {
+			if project.Name == projectname {
+				return &project.ScopedToken, nil
+			}
+		}
+
+	}
+
+	return nil, fmt.Errorf("no such Project \"%v\"", projectname)
 }
