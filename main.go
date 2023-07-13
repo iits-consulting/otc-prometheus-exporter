db5a56ea7865ee05b314a909f370cd887b8f0833
erjondibranica <erjon@cloudastro.de>
2023-07-13 19:19:24 +0200

    implement flags with env fallback

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	cmdPackage "github.com/iits-consulting/otc-prometheus-exporter/cmd"
 	"os"
+
+	cmdPackage "github.com/iits-consulting/otc-prometheus-exporter/cmd"
 
 	"golang.org/x/exp/maps"
 	"golang.org/x/exp/slices"
@@ -110,22 +112,71 @@ func FetchResourceIdToNameMapping(client *internal.OtcWrapper, namespaces []stri
 }
 
 func main() {
-	var port uint16
+	var (
+		port                  uint16
+		region                string
+		namespaces            []string
+		username              string
+		password              string
+		accessKey             string
+		secretKey             string
+		projectId             string
+		osDomainName          string
+		fetchResourceIdToname bool
+		waitDuration          time.Duration
+	)
 
 	var rootCmd = &cobra.Command{
 		Use: "otc-prometheus-exporter",
 		PreRunE: func(cmd *cobra.Command, args []string) error {
+			fmt.Println(region)
 			return cmdPackage.InitializeConfig(cmd, map[string]string{
-				"port": "PORT",
+				"port":                  "PORT",
+				"region":                "REGION",
+				"namespaces":            "NAMESPACES",
+				"os_Username":           "Os_USERNAME",
+				"os_Password":           "Os_PASSWORD",
+				"os_access_key":         "OS_ACCESS_KEY",
+				"os_secret_key":         "OS_SECRET_KEY",
+				"projectId":             "OS_PROJECT_ID",
+				"osDomainName":          "OS_DOMAIN_NAME",
+				"waitDuration":          "WAITDURATION",
+				"fetchResourceIdToname": "FETCH_RESOURCE_ID_TO_NAME",
 			})
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
+			isAkSkAuthentication := false
+			switch {
+			case username != "" && password != "":
+				isAkSkAuthentication = false
+			case accessKey != "" && secretKey != "":
+				isAkSkAuthentication = true
+			default:
+				return errors.New("no valid authentication data provided. please provide either username and password or accessKey and secretKey")
+			}
+			fmt.Println(isAkSkAuthentication)
+			otcRegion, err := internal.NewOtcRegionFromString(region)
+			if err != nil {
+				return err
+			}
+			fmt.Println(otcRegion)
 			fmt.Println(">>>>", port)
+			fmt.Println(region)
+			fmt.Println(namespaces, username, password, accessKey, secretKey, waitDuration, fetchResourceIdToname)
 			return nil
 		},
 	}
-
-	rootCmd.Flags().Uint16VarP(&port, "port", "p", 39100, "Port on which metrics are served")
+	rootCmd.Flags().Uint16VarP(&port, "port", "", 39100, "Port on which metrics are served")
+	rootCmd.Flags().StringVarP(&region, "region", "r", "eu-de", "region where your project is located ")
+	rootCmd.Flags().StringSliceVarP(&namespaces, "namespaces", "n", maps.Values(internal.OtcNamespacesMapping), "namespaces for instances you want to get the metrics from")
+	rootCmd.Flags().StringVarP(&username, "username", "u", "", "user in the OTC with access to the API. Must be provided together with password and can't be provided with AK/SK")
+	rootCmd.Flags().StringVarP(&password, "password", "p", "", "password for the user. Must be provided together with username and can't be provided with AK/SK")
+	rootCmd.Flags().StringVarP(&accessKey, "accessKey", "", "", "you can instead of username/password also provide the users AK and SK")
+	rootCmd.Flags().StringVarP(&secretKey, "secretKey", "", "", "you can instead of username/password also provide the users AK and SK")
+	rootCmd.Flags().StringVarP(&projectId, "projectId", "", "", "project from which the metrics should be gathered")
+	rootCmd.Flags().StringVarP(&osDomainName, "osDomainName", "", "", "Domainname/Tenant ID")
+	rootCmd.Flags().BoolVarP(&fetchResourceIdToname, "fetchResourceIdToname", "", false, "turns the mapping of resource id to resource name on or off")
+	rootCmd.Flags().DurationVarP(&waitDuration, "waitDuration", "", 60*time.Second, "time in seconds between two API call fetches")
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
